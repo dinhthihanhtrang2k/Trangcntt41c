24225899a45603f61972c25fd593f1976994d522
dinhthihanhtrang2k <56387200+dinhthihanhtrang2k@users.noreply.github.com>
2019-11-07 16:31:13 +0700

    Add files via upload

---
+++ VDAWT1.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+public class VDAWT1 {
+    public static void main(String args[]){
+        Frame f = new Frame("Chao");
+        Label l = new Label("Chao Java - AWT");
+        Font fo = new Font("Arial",Font.BOLD,20);
+        l.setFont(fo);
+    l.setForeground(Color.red);
+        f.add(l);
+        f.setSize(200,100);
+        f.setVisible(true);
+                                                                                                                                                                                                                                                                                                                                                            
+        
+    }
+    
+}
